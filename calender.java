350d8ab449db79071ceee832a0abec6ca988dd82
kadubhumika <kadubhumika@gmail.com>
2025-05-27 15:10:31 +0530

    Added new code

---
+++ calender.java
@@ -0,0 +1,18 @@
+public class CalendarProgram {
+    public static void main(String[] args) {
+        calendar();
+    }
+
+    public static void calendar() {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for (int i = 0; i < 12; i++) {
+            System.out.println(months[i] + ":");
+            for (int j = 1; j <= daysInMonth[i]; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
